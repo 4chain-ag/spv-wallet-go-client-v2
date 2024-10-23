c877e111caf0a6d75602e19a11068bf815edce74
mgosek-4chain <michal.gosek@4chain.studio>
2024-10-23 14:22:21 +0200

    refactor(SPV-1087): remove dependency to spv-wallet, add get child nums from hex func.

--- go.mod
+++ go.mod
@@ -8,14 +8,8 @@ require (
 )
 
 require (
-	github.com/bitcoinschema/go-bitcoin/v2 v2.0.5 // indirect
-	github.com/bitcoinsv/bsvd v0.0.0-20190609155523-4c29707f7173 // indirect
-	github.com/libsv/go-bk v0.1.6 // indirect
-	github.com/libsv/go-bt/v2 v2.2.5 // indirect
-)
-
-require (
-	github.com/bitcoin-sv/spv-wallet/engine v0.27.3
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
 )
--- internal/cryptoutil/cryptoutil.go
+++ internal/cryptoutil/cryptoutil.go
@@ -4,9 +4,12 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
+	"errors"
+	"fmt"
+	"math"
+	"strconv"
 
 	bip32 "github.com/bitcoin-sv/go-sdk/compat/bip32"
-	"github.com/bitcoin-sv/spv-wallet/engine/utils"
 )
 
 const (
@@ -43,7 +46,7 @@ func DeriveChildKeyFromHex(hdKey *bip32.ExtendedKey, hexHash string) (*bip32.Ext
 	var childKey *bip32.ExtendedKey
 	childKey = hdKey
 
-	childNums, err := utils.GetChildNumsFromHex(hexHash)
+	childNums, err := GetChildNumsFromHex(hexHash)
 	if err != nil {
 		return nil, err
 	}
@@ -55,3 +58,52 @@ func DeriveChildKeyFromHex(hdKey *bip32.ExtendedKey, hexHash string) (*bip32.Ext
 	}
 	return childKey, nil
 }
+
+// GetChildNumsFromHex get an array of uint32 numbers from the hex string
+func GetChildNumsFromHex(hexHash string) ([]uint32, error) {
+	strLen := len(hexHash)
+	size := 8
+	splitLength := int(math.Ceil(float64(strLen) / float64(size)))
+	childNums := make([]uint32, 0)
+	for i := 0; i < splitLength; i++ {
+		start := i * size
+		stop := start + size
+		if stop > strLen {
+			stop = strLen
+		}
+		num, err := strconv.ParseInt(hexHash[start:stop], 16, 64)
+		if err != nil {
+			return nil, fmt.Errorf("parse int op failure: %w", err)
+		}
+
+		result := num % MaxInt32
+		resultU32, err := Int64ToUint32(result)
+		if err != nil {
+			return nil, fmt.Errorf("int64 to uint32 convert op failure: %w", err)
+		}
+		childNums = append(childNums, resultU32)
+	}
+
+	return childNums, nil
+}
+
+// Int64ToUint32 converts an int64 value to uint32 with range checks.
+// It returns the converted uint32 value and a nil error if the conversion is successful.
+// Otherwise, it returns the zero value and a non-nil error that describes the reason for the conversion failure.
+func Int64ToUint32(value int64) (uint32, error) {
+	if value < 0 {
+		return 0, ErrNegativeValueNotAllowed
+	}
+	if value > math.MaxUint32 {
+		return 0, ErrMaxUint32LimitExceeded
+	}
+	return uint32(value), nil
+}
+
+var (
+	// ErrMaxUint32LimitExceeded occurs when attempting to convert an int64 value that exceeds the maximum uint32 limit.
+	ErrMaxUint32LimitExceeded = errors.New("max uint32 value exceeded")
+
+	// ErrNegativeValueNotAllowed occurs when attempting to convert a negative int64 value to uint32.
+	ErrNegativeValueNotAllowed = errors.New("negative value is not allowed")
+)

22475b4b805491480b70f5f6f2355693d284d196
mgosek-4chain <michal.gosek@4chain.studio>
2024-10-24 09:52:19 +0200

    refactor(SPV-1087): add unit tests and documentation for cryptoutil pkg.

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.5
 require (
 	github.com/bitcoin-sv/go-sdk v1.1.9
 	github.com/bitcoin-sv/spv-wallet/models v1.0.0-beta.31
+	github.com/stretchr/testify v1.9.0
 )
 
 require (
@@ -12,4 +13,5 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/cryptoutil/cryptoutil.go
+++ internal/cryptoutil/cryptoutil.go
@@ -13,83 +13,97 @@ import (
 )
 
 const (
-	// XpubKeyLength is the length of an xPub string key
+	// XpubKeyLength is the length of an xPub string key.
 	XpubKeyLength = 111
 
-	// ChainInternal internal chain num
+	// ChainInternal internal chain num.
 	ChainInternal = uint32(1)
 
-	// ChainExternal external chain num
+	// ChainExternal external chain num.
 	ChainExternal = uint32(0)
-
-	// MaxInt32 max integer for int32
-	MaxInt32 = int64(1<<(32-1) - 1)
 )
 
-// Hash returns the sha256 hash of the data string
-func Hash(data string) string {
-	hash := sha256.Sum256([]byte(data))
-	return hex.EncodeToString(hash[:])
+// Hash returns encoded string from SHA256 checksum of the s value.
+func Hash(s string) string {
+	bb := sha256.Sum256([]byte(s))
+	return hex.EncodeToString(bb[:])
 }
 
-// RandomHex returns a random hex string and error
+// RandomHex returns a random hexadecimal string of length `n`.
+// An non-nil error is returned when random byte generation process failure occurs (rand.Read).
 func RandomHex(n int) (string, error) {
-	b := make([]byte, n)
-	if _, err := rand.Read(b); err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(b), nil
-}
-
-// DeriveChildKeyFromHex derive the child extended key from the hex string
-func DeriveChildKeyFromHex(hdKey *bip32.ExtendedKey, hexHash string) (*bip32.ExtendedKey, error) {
-	var childKey *bip32.ExtendedKey
-	childKey = hdKey
-
-	childNums, err := GetChildNumsFromHex(hexHash)
+	bb := make([]byte, n)
+	_, err := rand.Read(bb)
 	if err != nil {
-		return nil, err
+		return "", fmt.Errorf("failed to read bytes after rand: %w", err)
 	}
-
-	for _, num := range childNums {
-		if childKey, err = childKey.Child(num); err != nil {
-			return nil, err
-		}
-	}
-	return childKey, nil
+	return hex.EncodeToString(bb), nil
 }
 
-// GetChildNumsFromHex get an array of uint32 numbers from the hex string
-func GetChildNumsFromHex(hexHash string) ([]uint32, error) {
-	strLen := len(hexHash)
-	size := 8
-	splitLength := int(math.Ceil(float64(strLen) / float64(size)))
-	childNums := make([]uint32, 0)
-	for i := 0; i < splitLength; i++ {
+// DeriveChildKeyFromHex derives a child extended key from a BIP32 master key using a hexadecimal hash.
+// The hexadecimal string is parsed into a 32-bit unsigned integers slice, each of which is used
+// to derive a successive child key from the provided master or parent key.
+func DeriveChildKeyFromHex(key *bip32.ExtendedKey, hexHash string) (*bip32.ExtendedKey, error) {
+	nums, err := ParseChildNumsFromHex(hexHash)
+	if err != nil {
+		return nil, fmt.Errorf("failed to return parsed child nums from hex hash: %w", err)
+	}
+
+	child := key
+	for _, n := range nums {
+		child, err = child.Child(n)
+		if err != nil {
+			return nil, fmt.Errorf("failed to return derived child extended key: %w", err)
+		}
+	}
+	return child, nil
+}
+
+// ParseChildNumsFromHex parses a hexadecimal string into multiple 32-bit unsigned integers.
+// The input hex string is divided into 8-character chunks, each of which is interpreted as a 32-bit
+// unsigned integer in hexadecimal format. The function returns a slice of these integers or an error
+// if any part of the hex string is not valid.
+func ParseChildNumsFromHex(hexHash string) ([]uint32, error) {
+	if hexHash == "" {
+		return nil, nil
+	}
+
+	const size = 8
+	parts := (len(hexHash) + size - 1) / size // Avoids the need for floating-point division and ensures correct rounding up.
+	var nums []uint32
+	for i := 0; i < parts; i++ {
 		start := i * size
-		stop := start + size
-		if stop > strLen {
-			stop = strLen
+		end := start + size
+		if end > len(hexHash) {
+			end = len(hexHash) // Adjust end to fit remaining substring.
 		}
-		num, err := strconv.ParseInt(hexHash[start:stop], 16, 64)
+		num, err := parseHexPart(hexHash[start:end])
 		if err != nil {
-			return nil, fmt.Errorf("parse int op failure: %w", err)
+			return nil, fmt.Errorf("failed to parse hex part %q: %w", hexHash[start:end], err)
 		}
-
-		result := num % MaxInt32
-		resultU32, err := Int64ToUint32(result)
-		if err != nil {
-			return nil, fmt.Errorf("int64 to uint32 convert op failure: %w", err)
-		}
-		childNums = append(childNums, resultU32)
+		nums = append(nums, num)
 	}
-
-	return childNums, nil
+	return nums, nil
 }
 
-// Int64ToUint32 converts an int64 value to uint32 with range checks.
-// It returns the converted uint32 value and a nil error if the conversion is successful.
-// Otherwise, it returns the zero value and a non-nil error that describes the reason for the conversion failure.
+// parseHexPart converts a hexadecimal string to a uint32 value.
+// The input string is expected to represent a valid hexadecimal number, and its value
+// must fit within the range of a 32-bit unsigned integer.
+func parseHexPart(part string) (uint32, error) {
+	i, err := strconv.ParseInt(part, 16, 64)
+	if err != nil {
+		return 0, errors.Join(err, ErrHexHashPartIntParse)
+	}
+	u, err := Int64ToUint32(i % math.MaxInt32)
+	if err != nil {
+		return 0, fmt.Errorf("failed to convert int64 to uint32: %w", err)
+	}
+	return u, nil
+}
+
+// Int64ToUint32 converts an int64 value to a uint32, ensuring that the input is within the valid range for a uint32.
+// The function performs a range check to ensure the int64 value is non-negative and does not exceed the maximum value
+// for a uint32 (which is 2^32 - 1).
 func Int64ToUint32(value int64) (uint32, error) {
 	if value < 0 {
 		return 0, ErrNegativeValueNotAllowed
@@ -106,4 +120,7 @@ var (
 
 	// ErrNegativeValueNotAllowed occurs when attempting to convert a negative int64 value to uint32.
 	ErrNegativeValueNotAllowed = errors.New("negative value is not allowed")
+
+	// ErrHexHashPartIntParse occurs when attempting to parse part of the hex hash to int64.
+	ErrHexHashPartIntParse = errors.New("parse hex hash part to int64 failed")
 )

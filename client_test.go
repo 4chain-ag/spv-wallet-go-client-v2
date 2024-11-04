ac43a7d0f52345bbf97af42fea665fe9f9841291
mgosek-4chain <michal.gosek@4chain.studio>
2024-11-04 09:03:22 +0100

    refactor(SPV-1087): move client_test file from internal pkg.

---
+++ client_test.go
@@ -0,0 +1,61 @@
+package client_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	client "github.com/bitcoin-sv/spv-wallet-go-client"
+	"github.com/bitcoin-sv/spv-wallet-go-client/internal/testutil"
+	"github.com/bitcoin-sv/spv-wallet/models"
+	"github.com/bitcoin-sv/spv-wallet/models/response"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestClient_OnAfterResponseErrMiddleware(t *testing.T) {
+	tests := map[string]struct {
+		statusCode       int
+		expectedResponse *response.SharedConfig
+		expectedErr      error
+		responder        httpmock.Responder
+	}{
+		"HTTP response: status 200": {
+			expectedResponse: &response.SharedConfig{},
+			statusCode:       http.StatusOK,
+			responder:        httpmock.NewJsonResponderOrPanic(http.StatusOK, &response.SharedConfig{}),
+		},
+		"HTTP error JSON response: status 400": {
+			expectedErr: models.SPVError{
+				Message:    http.StatusText(http.StatusBadRequest),
+				StatusCode: http.StatusBadRequest,
+				Code:       "invalid-data-format",
+			},
+			statusCode: http.StatusOK,
+			responder: httpmock.NewJsonResponderOrPanic(http.StatusBadRequest, &models.SPVError{
+				Message:    http.StatusText(http.StatusBadRequest),
+				StatusCode: http.StatusBadRequest,
+				Code:       "invalid-data-format",
+			}),
+		},
+		"HTTP error str response: status 500": {
+			expectedErr: client.ErrUnrecognizedAPIResponse,
+			statusCode:  http.StatusInternalServerError,
+			responder:   httpmock.NewStringResponder(http.StatusInternalServerError, "unexpected internal server failure"),
+		},
+	}
+
+	URL := testutil.TestAPIAddr + "/api/v1/configs/shared"
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			// when:
+			cli, transport := testutil.NewMockSPVClient(t)
+			transport.RegisterResponder(http.MethodGet, URL, tc.responder)
+
+			// then:
+			got, err := cli.SharedConfig(context.Background())
+			require.ErrorIs(t, err, tc.expectedErr)
+			require.Equal(t, tc.expectedResponse, got)
+		})
+	}
+}

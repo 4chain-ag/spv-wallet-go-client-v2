e67709acfeb24f8ade331282349e1d371a904f79
Augustyn Chmiel <augustyn.chmiel@acoustic.com>
2024-12-09 12:35:43 +0100

    refact(spv-1163) uncommenting example code

--- examples/accept_invitation/accept_invitation.go
+++ examples/accept_invitation/accept_invitation.go
@@ -1,39 +1,25 @@
 package main
 
-// import (
-// 	"context"
-// 	"log"
-//
-//
-
-// 	wallet "github.com/bitcoin-sv/spv-wallet-go-client"
-// 	"github.com/bitcoin-sv/spv-wallet-go-client/examples"
-// 	"github.com/bitcoin-sv/spv-wallet-go-client/examples/exampleutil"
-// )
-
-// func main() {
-// 	usersAPI, err := wallet.NewUserAPIWithXPriv(exampleutil.NewDefaultConfig(), examples.XPriv)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	paymail := "john.doe@example.com"
-// 	err = usersAPI.AcceptInvitation(context.Background(), paymail)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	exampleutil.Print("\n[HTTP POST] Accept contact invitation - api/v1/invitations/%s/contacts", paymail)
-// }
-
 import (
-	"fmt"
+	"context"
+	"log"
 
-	"github.com/bitcoin-sv/spv-wallet-go-client/config"
+	wallet "github.com/bitcoin-sv/spv-wallet-go-client"
+	"github.com/bitcoin-sv/spv-wallet-go-client/examples"
+	"github.com/bitcoin-sv/spv-wallet-go-client/examples/exampleutil"
 )
 
 func main() {
-	cfg := config.New()
+	usersAPI, err := wallet.NewUserAPIWithXPriv(exampleutil.NewDefaultConfig(), examples.XPriv)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Printf("This is config %+v", cfg)
+	paymail := "john.doe@example.com"
+	err = usersAPI.AcceptInvitation(context.Background(), paymail)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exampleutil.Print("\n[HTTP POST] Accept contact invitation - api/v1/invitations/%s/contacts", paymail)
 }

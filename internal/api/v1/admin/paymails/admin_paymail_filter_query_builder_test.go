231f7a48e5c7950fb5283f8bf864ebab2418bda8
mgosek-4chain <michal.gosek@4chain.studio>
2024-12-10 09:49:00 +0100

    fix(SPV-1264): update unit test name for admin paymail filter query builder.

--- internal/api/v1/admin/paymails/admin_paymail_filter_query_builder_test.go
+++ internal/api/v1/admin/paymails/admin_paymail_filter_query_builder_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestAdminAccessKeyFilterQueryBuilder_Build(t *testing.T) {
+func TesAdminPaymailFilterQueryBuilder_Build(t *testing.T) {
 	tests := map[string]struct {
 		filter         filter.AdminPaymailFilter
 		expectedParams url.Values

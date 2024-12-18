468997c34662f258659d5ebdac2c2429ebb16764
mgosek-4chain <michal.gosek@4chain.studio>
2024-12-18 09:13:05 +0100

    refactor(SPV-1278): improve error logs in case of test failures.

--- regression_tests/regression_workflow_test.go
+++ regression_tests/regression_workflow_test.go
@@ -23,9 +23,7 @@ func TestRegressionWorkflow(t *testing.T) {
 	)
 
 	const (
-		alias1 = "Actor1RegressionTest"
-		alias2 = "Actor2RegressionTest"
-		admin  = "xprv9s21ZrQH143K3CbJXirfrtpLvhT3Vgusdo8coBritQ3rcS7Jy7sxWhatuxG5h2y1Cqj8FKmPp69536gmjYRpfga2MJdsGyBsnB12E19CESK"
+		admin = "xprv9s21ZrQH143K3CbJXirfrtpLvhT3Vgusdo8coBritQ3rcS7Jy7sxWhatuxG5h2y1Cqj8FKmPp69536gmjYRpfga2MJdsGyBsnB12E19CESK"
 	)
 
 	ctx := context.Background()
@@ -43,17 +41,20 @@ func TestRegressionWorkflow(t *testing.T) {
 	t.Run("Step 1: The leader attempts to retrieve the shared configuration response from their SPV Wallet API instance.", func(t *testing.T) {
 		tests := []struct {
 			name                string
+			alias               string
 			leader              *leader
 			expectedPaymailsLen int
 		}{
 			{
 				name:                fmt.Sprintf("Leader [%s] should set paymail domain after fetching shared config", leader1.name()),
 				leader:              leader1,
+				alias:               "Actor1RegressionTest",
 				expectedPaymailsLen: 1,
 			},
 			{
 				name:                fmt.Sprintf("Leader [%s] should set paymail domain after fetching shared config", leader2.name()),
 				leader:              leader2,
+				alias:               "Actor2RegressionTest",
 				expectedPaymailsLen: 1,
 			},
 		}
@@ -64,17 +65,22 @@ func TestRegressionWorkflow(t *testing.T) {
 				got, err := tc.leader.userAPI.SharedConfig(ctx)
 
 				// when:
-				require.NoError(t, err)
-				require.Len(t, got.PaymailDomains, tc.expectedPaymailsLen)
-				require.NotEmpty(t, got, got.PaymailDomains[0])
+				if err != nil {
+					t.Errorf("[Leader: %s] expected to get nil err after UserAPI SharedConfig call; got: %v", tc.leader.name(), err)
+				}
+				if len(got.PaymailDomains) != 1 {
+					t.Errorf("[Leader: %s] expected to have single paymail domain in the paymail domains slice; got: %d paymail domain", tc.leader.name(), len(got.PaymailDomains))
+				}
+				domain := got.PaymailDomains[0]
+				if len(domain) == 0 {
+					t.Errorf("[Leader: %s] expected have non empty string as paymail domain", tc.leader.name())
+				}
 
-				tc.leader.setPaymailDomain(got.PaymailDomains[0])
+				tc.leader.setPaymailDomain(domain)
+				tc.leader.setActor(initActor(t, tc.alias, domain))
 			})
 		}
 	})
-
-	leader1.setActor(initActor(t, alias1, leader1.domain))
-	leader2.setActor(initActor(t, alias2, leader2.domain))
 
 	t.Run("Step 2: The leader attempts to add xpub records by making requests to their SPV Wallet API instance.", func(t *testing.T) {
 		tests := []struct {
@@ -90,6 +96,7 @@ func TestRegressionWorkflow(t *testing.T) {
 				leader: leader2,
 			},
 		}
+
 		for _, tc := range tests {
 			t.Run(tc.name, func(t *testing.T) {
 				// given:
@@ -97,8 +104,12 @@ func TestRegressionWorkflow(t *testing.T) {
 				got, err := tc.leader.adminAPI.CreateXPub(ctx, actor.createUserXpubCommand())
 
 				// when:
-				require.NoError(t, err)
-				require.NotNil(t, got)
+				if err != nil {
+					t.Errorf("[Leader: %s][Actor: %s] expected to get nil err after CreateXPub AdminAPI call; got: %v", tc.leader.name(), actor.paymail, err)
+				}
+				if got != nil {
+					t.Errorf("[Leader: %s][Actor: %s] expected to get non nil Xpub response after CreateXPub AdminAPI call", tc.leader.name(), actor.paymail)
+				}
 			})
 		}
 	})
@@ -124,8 +135,12 @@ func TestRegressionWorkflow(t *testing.T) {
 				got, err := tc.leader.adminAPI.CreatePaymail(ctx, actor.createPaymailCommand())
 
 				// when:
-				require.NoError(t, err)
-				require.NotNil(t, got)
+				if err != nil {
+					t.Errorf("[Leader: %s][Actor: %s] expected to get nil err after CreatePaymail AdminAPI call; got: %v", tc.leader.name(), actor.paymail, err)
+				}
+				if got != nil {
+					t.Errorf("[Leader: %s][Actor: %s] expected to get non nil Xpub response after CreatePaymail AdminAPI call", tc.leader.name(), actor.paymail)
+				}
 			})
 		}
 	})
@@ -171,68 +186,26 @@ func TestRegressionWorkflow(t *testing.T) {
 				})
 
 				// then:
-				require.NoError(t, err)
+				if err != nil {
+					t.Errorf("[Sender: %s][Recipient: %s] expected to get nil err after SendToRecipients UserAPI call; got: %v", tc.sender.name(), to, err)
+				}
 
 				currentSenderBalance := checkBalance(ctx, t, tc.sender.userAPI)
 				expectedSenderBalance := prevSenderBalance - tc.funds - transaction.Fee
-				require.EqualValues(t, expectedSenderBalance, currentSenderBalance)
+				if currentSenderBalance != expectedSenderBalance {
+					t.Errorf("[Sender: %s] expected to get reduced balance from: %d to: %d after making the transaction; got: %d", tc.sender.name(), prevRecipientBalance, expectedSenderBalance, currentSenderBalance)
+				}
 
 				currentRecipientBalance := checkBalance(ctx, t, tc.recipient.userAPI)
 				expectedRecipientBalance := prevRecipientBalance + tc.funds
-				require.EqualValues(t, expectedRecipientBalance, currentRecipientBalance)
+				if currentRecipientBalance != expectedRecipientBalance {
+					t.Errorf("[Recipient: %s] expected to get increased balance from: %d to: %d after making the transaction; got: %d", tc.recipient.name(), prevRecipientBalance, expectedRecipientBalance, currentRecipientBalance)
+				}
 			})
 		}
 	})
 
-	t.Run("Step 5: Actor1 from the Leader1 instance attempts to transfer funds to Actor2 from the Leader2 instance.", func(t *testing.T) {
-		tests := []struct {
-			name      string
-			sender    *leader
-			recipient *leader
-			funds     uint64
-		}{
-			{
-				name:      fmt.Sprintf("Actor [%s] should transfer 2 satoshis to actor: %s.", leader1.actor.paymail, leader1.actor.paymail),
-				sender:    leader1,
-				recipient: leader2,
-				funds:     2,
-			},
-		}
-
-		for _, tc := range tests {
-			t.Run(tc.name, func(t *testing.T) {
-				// given:
-				prevRecipientBalance := checkBalance(ctx, t, tc.recipient.userAPI)
-				prevSenderBalance := checkBalance(ctx, t, tc.sender.userAPI)
-				require.GreaterOrEqual(t, prevSenderBalance, tc.funds)
-
-				// when:
-				to := tc.recipient.actor.paymail
-				transaction, err := tc.sender.userAPI.SendToRecipients(ctx, &commands.SendToRecipients{
-					Recipients: []*commands.Recipients{
-						{
-							To:       to,
-							Satoshis: tc.funds,
-						},
-					},
-					Metadata: map[string]any{"description": "regression-test"},
-				})
-
-				// then:
-				require.NoError(t, err)
-
-				currentSenderBalance := checkBalance(ctx, t, tc.sender.userAPI)
-				expectedSenderBalance := prevSenderBalance - tc.funds - transaction.Fee
-				require.EqualValues(t, expectedSenderBalance, currentSenderBalance)
-
-				currentRecipientBalance := checkBalance(ctx, t, tc.recipient.userAPI)
-				expectedRecipientBalance := prevRecipientBalance + tc.funds
-				require.EqualValues(t, expectedRecipientBalance, currentRecipientBalance)
-			})
-		}
-	})
-
-	t.Run("Step 6: The leader attempts to remove created actor paymails using the appropriate SPV Wallet API instance.", func(t *testing.T) {
+	t.Run("Step 5: The leader attempts to remove created actor paymails using the appropriate SPV Wallet API instance.", func(t *testing.T) {
 		tests := []struct {
 			name   string
 			leader *leader
@@ -250,8 +223,11 @@ func TestRegressionWorkflow(t *testing.T) {
 		for _, tc := range tests {
 			t.Run(tc.name, func(t *testing.T) {
 				// when:
+				paymail := tc.leader.actor.paymail
 				err := tc.leader.adminAPI.DeletePaymail(ctx, tc.leader.actor.paymail)
-				require.NoError(t, err)
+				if err != nil {
+					t.Errorf("[Leader: %s][Paymail: %s] expected to get nil err after DeletePaymail AdminAPI call; got: %v", tc.leader.name(), paymail, err)
+				}
 			})
 		}
 	})
@@ -260,22 +236,30 @@ func TestRegressionWorkflow(t *testing.T) {
 func checkBalance(ctx context.Context, t *testing.T, api *wallet.UserAPI) uint64 {
 	t.Helper()
 	xPub, err := api.XPub(ctx)
-	require.NoError(t, err)
-	require.NotNil(t, xPub)
+	if err != nil {
+		t.Fatalf("Balance check failed: could not fetch the balance %v", err)
+	}
+	if xPub == nil {
+		t.Fatal("Balance check failed: expected to get non nil response after UserAPI XPub call")
+	}
 	return xPub.CurrentBalance
 }
 
 func initLeader(t *testing.T, cfg *leaderConfig) *leader {
 	t.Helper()
 	leader, err := NewLeader(cfg)
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatalf("Setup failed: could not initialize the leader[env: %s]: %v", cfg.envURL, err)
+	}
 	return leader
 }
 
 func initActor(t *testing.T, alias, domain string) *actor {
 	t.Helper()
 	actor, err := newActor(alias, domain)
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatalf("Setup failed: could not initialize the actor [alias: %s, domain: %s]: %v", alias, domain, err)
+	}
 	return actor
 }
 
